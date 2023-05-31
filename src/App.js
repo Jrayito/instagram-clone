1ae07f91d0b91f75bf17d246b8429dfb5f73167f
Jesus Raymundo <jesusraymundi@gmail.com>
2023-05-31 11:38:46 -0600

    Se agrego el componente main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Main from "./interfaces/main";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
