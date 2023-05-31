e9e3eeafbdb52ea9d90142cbe32b9d2e46f7f9aa
Jesus Raymundo <jesusraymundi@gmail.com>
2023-05-31 12:51:47 -0600

    Se agrego el componente navigator

--- src/interfaces/main.jsx
+++ src/interfaces/main.jsx
@@ -1,9 +1,11 @@
 import Nav from "../componentes/nav";
+import Navigator from "../componentes/navigator";
 
 const Main = () => {
   return (
     <main>
       <Nav />
+      <Navigator />
     </main>
   );
 };
